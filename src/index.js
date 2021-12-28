1df676a0c26b21535959c6a3a363a1fa9ecb7e90
pooh <cktjdgus45@naver.com>
2021-12-28 19:02:26 +0900

    add youtube object

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import Youtube from './service/youtube.js';
+
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/service/youtube.js
@@ -0,0 +1,26 @@
+class Youtube {
+    constructor(key) {
+        this.baseurl = "https://www.googleapis.com/youtube/v3";
+        this.requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        this.key = key;
+    }
+
+    async search(query) {
+        const response = await fetch(`${this.baseurl}/search?part=snippet&maxResults=25&q=${query}&key=${this.key}`, this.requestOptions);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+
+    async mostPopularVideo() {
+        const response = await fetch(`${this.baseurl}/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`, this.requestOptions);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }
+}
+
+export default Youtube;
