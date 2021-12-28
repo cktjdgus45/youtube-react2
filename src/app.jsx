dc436a44ecf5906592f9f1fdb1207d8db4449f8b
pooh <cktjdgus45@naver.com>
2021-12-28 15:18:06 +0900

    add app jsx,css

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import './app.module.css';
-import SearchHeader from './components/search_header';
+import SearchHeader from './components/search_header/search_header';
+import VideoList from './components/video_list/video_list';
 
 function App() {
   return (
     <div className="App">
       <SearchHeader />
+      <VideoList />
     </div>
   );
 }
